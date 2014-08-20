91be8410ffcf5ee5ceb2d4713046d21fb632f18d
Weien Wang <weienw@gmail.com>
2014-08-19 23:16:21 -0500

    [NEW] SponsorsVC (with adjusted sponsor icon)

---
+++ DataTransparencyConference/SponsorsViewController.h
@@ -0,0 +1,13 @@
+//
+//  SponsorsViewController.h
+//  DataTransparency2014
+//
+//  Created by Weien Wang on 8/19/14.
+//  Copyright (c) 2014 Weien. All rights reserved.
+//
+
+#import "DTCViewController.h"
+
+@interface SponsorsViewController : DTCViewController
+
+@end
