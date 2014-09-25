2d98db4febf3b5f04efe878b66e89671dc65fbff
Weien Wang <weienw@gmail.com>
2014-09-25 14:11:03 -0500

    GINORMO FIX -- recreate entire project so that DT2014 will work with Xcode 6/iOS 8

---
+++ DataTransparency2014TEST/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DataTransparency2014TEST
+//
+//  Created by Weien Wang on 9/13/14.
+//  Copyright (c) 2014 Weien Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
